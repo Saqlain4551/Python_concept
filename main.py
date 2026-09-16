092249e7630fe56dbbf1f24ef91d8c54864fe8bb
Saqlain4551 <m.maniyar@somaiya.edu>
2026-09-16 11:38:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+'''DEVELOP BY MOAHMMED SAQLAIN MANIYAR 
+PYTHON BASIC CONCEPT ''' 
+
+print("Develop by Mohammed Saqlain")
+print("hello world")
+print(12*17)
+print("I am start learning python from today, my day 01 python is start now ")
+print(12*56,23+1,90/2,65-34)
+
+print("I learn python for build my own app\n and getting gob in good and highy position level\n If i will sucess according to my plan then i also planned i will open my own company or office ")
